f4bbc1dbdcc93d678964a78e6c6609379904fcba
Ldeng928 <lideng928@gmail.com>
2021-12-11 14:31:35 -0500

    Challenge 20 code update

--- challenge20.js
+++ challenge20.js
@@ -4,33 +4,28 @@ Your task is to write a function that will take in speed (number), missionData (
 Speed will be compared against maxSpeed and minSpeed inclusively and the length of each array inside missionData will be compared to the values inside the dataEntries object values. 
 */
 
-const confirmReentryPlans = (speed, missionData, checks) => {
-  // Code here!
-  maxSpeed = checks.maxSpeed;
-  minSpeed = checks.minSpeed;
-  if (missionData.astro) {
-    astroLength = missionData.astro.length;
-  }
-  if (missionData.bio) {
-    bioLength = missionData.bio.length;
-  }
-  if (missionData.physics) {
-    physicsLength = missionData.physics.length;
-  }
-
-  checkAstroLength = checks.dataEntries.astro;
-  checkBioLength = checks.dataEntries.bio;
-  checkPhysicsLength = checks.dataEntries.physics;
-
-  if (speed >= minSpeed && speed <= maxSpeed && astroLength === checkAstroLength && bioLength === checkBioLength && physicsLength === checkPhysicsLength) {
-    return true;
-  } else {
+// Utility function to determine if two objects are equal
+function areEqual(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
     return false;
   }
+  for (let key of keys1) {
+    if (object1[key] !== object2[key].length) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const confirmReentryPlans = (speed, missionData, checks) => {
+  // Code here!
+  return speed <= checks.maxSpeed && speed >= checks.minSpeed && areEqual(checks.dataEntries, missionData);
 };
 
 // Example - everything is right
-const speed = 40;
+const speed = 60;
 const missionData = {
   astro: ['...', '...'],
   bio: ['...'],
